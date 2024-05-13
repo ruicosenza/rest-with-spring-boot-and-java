1529f85179ecd00d20ddbda803884cf826a76dda
Rui Cosenza <ruicosenza@gmail.com>
2024-05-13 16:03:34 -0300

    First Steps in Java with Spring boot 3

---
+++ 05_FirstStesInJavaWithSpringBoot/rest-with-spring-boot-and-java/src/test/java/br/com/cosenza/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.cosenza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
